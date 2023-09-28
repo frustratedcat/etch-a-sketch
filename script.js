df8478c4e4e473259e3e141153c400aa7ae0ab97
frustratedcat <jaredbrown@skiff.com>
2023-09-28 10:46:03 -0400

    Add initial containers and buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let btnSizeContainer = document.querySelectorAll(".btn-size-container");
+const btn16 = document.querySelector(".btn-16");
+const btn32 = document.querySelector(".btn-32");
+const btn48 = document.querySelector(".btn-48");
+
+const gameContainer = document.querySelector(".game-container");
